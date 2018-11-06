47e67750af2e854649729eae959b6dce3b1c8781
Purshotam Kumar <itmanager@lamars.local>
2018-11-05 20:27:02 -0700

    first commit

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v4.6.6',
+  'full_app_version' => 'v4.6.6 - build 3926-g1a10aa0dd',
+  'build_version' => '3926',
+  'prerelease_version' => '',
+  'hash_version' => 'g1a10aa0dd',
+  'full_hash' => 'v4.6.6-24-g1a10aa0dd',
+  'branch' => 'master',
+);
